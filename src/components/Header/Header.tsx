b8ac171c06d68bb0a69b20c5f0c735b9d27f0c8c
Evan Charlton <evancharlton@gmail.com>
2023-10-18 19:17:37 +0200

    Add logos and dialog text

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
 import classes from "./Header.module.css";
 import { Dialog } from "../Dialog";
-import { MdHelpOutline, MdOutlineSettings } from "react-icons/md";
+import {
+  MdHelpOutline,
+  MdOutlineAutorenew,
+  MdOutlineSettings,
+} from "react-icons/md";
 import { Link, useParams } from "react-router-dom";
 
 type Props = {
@@ -31,19 +35,47 @@ export const Header = ({ buttons }: Props) => {
           <button onClick={() => setDialog("help")}>
             <MdHelpOutline />
           </button>
-          <button onClick={() => setDialog("settings")}>
-            <MdOutlineSettings />
-          </button>
         </div>
       </div>
       {dialog === "help" && (
         <Dialog title="Hjelp" onClose={() => setDialog(undefined)}>
-          <p>Help text here</p>
+          <p>
+            <strong>Bokstav Boks</strong> er et spill med norske ord. Fullfør
+            puslespillet ved å lage ord som bruker alle 12 bokstavene. Det er
+            lov å bruke bokstaver flere ganger! Hver bokstav må imidlertid være
+            på en annen side enn den forrige bokstaven.
+          </p>
+          <p>Lykke til!</p>
+          <p>
+            Du kan spille så mange oppgaver du vil, og så lenge du vil. Klikk på{" "}
+            <span
+              style={{
+                border: "1px solid black",
+                borderRadius: 4,
+                display: "inline-flex",
+                alignItems: "center",
+              }}
+            >
+              <MdOutlineAutorenew />
+            </span>{" "}
+            knappene for å generere et nytt puslespill!
+          </p>
+          <hr />
+          <p>
+            (Want to play this in English? Try{" "}
+            <a
+              href="https://www.nytimes.com/puzzles/letter-boxed"
+              rel="nofollow noopener noreferrer"
+            >
+              NYT Letter Boxed
+            </a>
+            !)
+          </p>
         </Dialog>
       )}
       {dialog === "settings" && (
         <Dialog title="Instillinger" onClose={() => setDialog(undefined)}>
-          <p>Settings dialog</p>
+          <p>TODO: Add this when I have settings to control</p>
         </Dialog>
       )}
     </>
