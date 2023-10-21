134d3c10c38ee53348b4661dec8b6e7dd4619c40
Evan Charlton <evancharlton@gmail.com>
2023-10-21 10:27:26 +0200

    Revert "Cache the word list locally too"
    This reverts commit 92b2bd017f802d26ef1179bfaee18e4dea00c807.

--- src/components/WordsProvider/WordsProvider.tsx
+++ src/components/WordsProvider/WordsProvider.tsx
@@ -2,8 +2,6 @@ import { useEffect, useMemo, useState } from "react";
 import { useParams } from "react-router-dom";
 import { WordsContext } from "./context";
 import { Loader } from "../Loader";
-import { useStorage } from "../../useStorage";
-import { isLetters } from "../../types";
 
 type Props = {
   children?: React.ReactNode;
@@ -14,40 +12,18 @@ export const WordsProvider = ({ children, words: initialWords }: Props) => {
   const { lang } = useParams();
   const [words, setWords] = useState<string[]>([]);
 
-  const wordBanks = useStorage("dicts");
-
   useEffect(() => {
     if (initialWords) {
       setWords(initialWords);
       return;
     }
 
-    wordBanks
-      .getItem("dictionary")
-      .then((list) => {
-        if (
-          list &&
-          Array.isArray(list) &&
-          list.every((item) => isLetters(item))
-        ) {
-          return list;
-        }
-
-        return fetch(`${process.env.PUBLIC_URL}/${lang}/words.json`)
-          .then((response) => response.json())
-          .then((words) => {
-            // TODO: Is there a better way to store this than one huge list
-            //       serialized as a key-value pair? This seems like it's
-            //       missing the point of IndexedDB ...
-            return wordBanks.setItem("dictionary", words).then(() => {
-              return words;
-            });
-          });
-      })
+    fetch(`${process.env.PUBLIC_URL}/${lang}/words.json`)
+      .then((response) => response.json())
       .then((words) => {
         setWords(words);
       });
-  }, [lang, initialWords, wordBanks]);
+  }, [lang, initialWords]);
 
   const value = useMemo(
     () => ({
