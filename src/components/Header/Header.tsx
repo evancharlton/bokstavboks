5bdeaa4978893b5f857cca17f790178249a45121
Evan Charlton <evancharlton@gmail.com>
2024-10-25 08:13:13 +0200

    fix: Move refresh button into the header
    The second-level banner didn't work too well - it can just live in the
    header.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,13 @@
 import classes from "./Header.module.css";
-import { MdHelpOutline, MdMenu } from "react-icons/md";
+import { MdHelpOutline, MdMenu, MdOutlineRefresh } from "react-icons/md";
 import { Link, useParams } from "react-router-dom";
 import { useDialog } from "../Dialogs";
+import { usePwa } from "../PwaContainer";
 
 export const Header = () => {
   const { lang = "" } = useParams();
   const { show } = useDialog();
+  const { updateNeeded, performUpdate } = usePwa();
 
   return (
     <>
@@ -14,6 +16,11 @@ export const Header = () => {
           <Link to={`/${lang}`}>Bokstavboks</Link>
         </h1>
         <div className={classes.buttons} style={{ gridArea: "right" }}>
+          {updateNeeded ? (
+            <button onClick={() => performUpdate()} className={classes.refresh}>
+              <MdOutlineRefresh />
+            </button>
+          ) : null}
           <button onClick={() => show("help")}>
             <MdHelpOutline />
           </button>
--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -1,5 +1,3 @@
-import { MdOutlineRefresh } from "react-icons/md";
-import { usePwa } from "../PwaContainer";
 import classes from "./Page.module.css";
 
 type Props = {
@@ -8,19 +6,9 @@ type Props = {
 };
 
 export const Page = ({ header, children }: Props) => {
-  const { updateNeeded, performUpdate } = usePwa();
-
   return (
     <div className={classes.container}>
       {header}
-      {updateNeeded ? (
-        <div className={classes.pwaContainer}>
-          oppdatering tilgjengelig
-          <button onClick={() => performUpdate()}>
-            <MdOutlineRefresh />
-          </button>
-        </div>
-      ) : null}
       <div className={classes.content}>{children}</div>
     </div>
   );
